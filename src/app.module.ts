e9cba608f8ecaf1b922d6ce8731b5ec192149302
Muzafar Umarov <umarov@users.noreply.github.com>
2020-06-21 19:57:58 -0400

    More process (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [ApiController, AppController]
 })
 export class AppModule {}
